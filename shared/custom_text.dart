ee8854b19c4c5d73690575b4abf077b370c20e74
tmdqls2257 <tmdqls2257@gmail.com>
2022-10-10 23:45:34 +0900

    🔨 text setting

---
+++ shared/custom_text.dart
@@ -0,0 +1,83 @@
+import 'package:flutter/material.dart';
+
+import 'theme.dart';
+
+enum TypoType {
+  h1Title,
+  h1Bold,
+  h1,
+  h2Bold,
+  h2,
+  body,
+  bodyLight,
+  bodySmaller,
+  label,
+  boldLabel,
+}
+
+class TypoStyle {
+  final FontWeight fontWeight;
+  final double fontSize;
+  final double letterSpacing;
+
+  TypoStyle({
+    required this.fontWeight,
+    required this.fontSize,
+    required this.letterSpacing,
+  });
+}
+
+final typoStyle = {
+  TypoType.h1Title:
+      TypoStyle(fontWeight: FontWeight.w900, fontSize: 32, letterSpacing: 0.48),
+  TypoType.h1Bold:
+      TypoStyle(fontWeight: FontWeight.w800, fontSize: 24, letterSpacing: 0.48),
+  TypoType.h1:
+      TypoStyle(fontWeight: FontWeight.w700, fontSize: 24, letterSpacing: 0.48),
+  TypoType.h2Bold:
+      TypoStyle(fontWeight: FontWeight.w700, fontSize: 18, letterSpacing: 0.32),
+  TypoType.h2:
+      TypoStyle(fontWeight: FontWeight.w400, fontSize: 18, letterSpacing: 0.32),
+  TypoType.body:
+      TypoStyle(fontWeight: FontWeight.w400, fontSize: 16, letterSpacing: 0.28),
+  TypoType.bodyLight:
+      TypoStyle(fontWeight: FontWeight.w300, fontSize: 16, letterSpacing: 0.28),
+  TypoType.bodySmaller:
+      TypoStyle(fontWeight: FontWeight.w300, fontSize: 14, letterSpacing: 0.28),
+  TypoType.label:
+      TypoStyle(fontWeight: FontWeight.w300, fontSize: 11, letterSpacing: 0.16),
+  TypoType.boldLabel:
+      TypoStyle(fontWeight: FontWeight.w500, fontSize: 11, letterSpacing: 0.16),
+};
+
+class CustomText extends SizedBox {
+  final String text;
+  final TypoType typoType;
+  final TextAlign textAlign;
+  final ColorType colorType;
+  final bool isFullWidth;
+  final bool inherit;
+
+  CustomText({
+    Key? key,
+    required this.text,
+    this.typoType = TypoType.body,
+    this.isFullWidth = false,
+    this.inherit = true,
+    this.textAlign = TextAlign.center,
+    this.colorType = ColorType.black,
+  }) : super(
+          key: key,
+          child: Text(
+            text,
+            textAlign: textAlign,
+            style: TextStyle(
+                inherit: inherit,
+                color: customColor[colorType],
+                fontWeight: typoStyle[typoType]!.fontWeight,
+                fontSize: typoStyle[typoType]!.fontSize,
+                letterSpacing: typoStyle[typoType]!.letterSpacing),
+            overflow: TextOverflow.visible,
+          ),
+        );
+}
